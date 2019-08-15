80bec6ec8dc4ae33f79e6649e2cbff4b32e12e26
radiegtya <radiegtya@gmail.com>
2019-08-15 19:07:05 +0700

    2.hello-express

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+    res.send('Hello Express!')
+})    
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
