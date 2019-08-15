0911786159de59bdb1209221893ada3c55c3551b
radiegtya <radiegtya@gmail.com>
2019-08-15 20:25:08 +0700

    4.routing

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-    res.send('Hello Express!')
-})    
+app.use(bodyParser.json())
+
+const todos = [
+    {
+        id: 1,
+        title: "Walking with Lucinta",
+        isDone: true
+    },
+    {
+        id: 2,
+        title: "Sleeping with Lucinta",
+        isDone: false
+    },
+]
+
+app.get('/todos', (req, res) => {
+    res.send(todos)
+})
+
+app.get('/todo/:id', (req, res) => {
+    const id = req.params.id
+    const index = id - 1    
+    res.send(todos[index])
+})
+
+app.post('/todo', (req, res) => {
+    const data = req.body
+    todos.push(data)
+    res.send(data)
+})
+
+app.patch('/todo/:id', (req, res) => {
+    const id = req.params.id
+    const index = id - 1 
+    const data = req.body    
+    todos[index] = {...todos[index], ...data}
+    res.send(todos[index])
+})
+
+app.delete('/todo/:id', (req, res) => {
+    const id = req.params.id
+    const index = id - 1        
+    todos.splice(index, 1)
+    res.send(todos)
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
