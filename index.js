ed6797de95120d22c4bd8f822755eb7c6244f8a6
radiegtya <radiegtya@gmail.com>
2019-08-15 21:05:02 +0700

    5.group-routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+require('express-group-routes')
 
 const app = express()
 const port = 5000
@@ -19,35 +20,39 @@ const todos = [
     },
 ]
 
-app.get('/todos', (req, res) => {
-    res.send(todos)
+app.group("/api/v1", (router) => {
+    router.get('/todos', (req, res) => {
+        res.send(todos)
+    })
+    
+    router.get('/todo/:id', (req, res) => {
+        const id = req.params.id
+        const index = id - 1    
+        res.send(todos[index])
+    })
+    
+    router.post('/todo', (req, res) => {
+        const data = req.body
+        todos.push(data)
+        res.send(data)
+    })
+    
+    router.patch('/todo/:id', (req, res) => {
+        const id = req.params.id
+        const index = id - 1 
+        const data = req.body    
+        todos[index] = {...todos[index], ...data}
+        res.send(todos[index])
+    })
+    
+    router.delete('/todo/:id', (req, res) => {
+        const id = req.params.id
+        const index = id - 1        
+        todos.splice(index, 1)
+        res.send(todos)
+    })
 })
 
-app.get('/todo/:id', (req, res) => {
-    const id = req.params.id
-    const index = id - 1    
-    res.send(todos[index])
-})
 
-app.post('/todo', (req, res) => {
-    const data = req.body
-    todos.push(data)
-    res.send(data)
-})
-
-app.patch('/todo/:id', (req, res) => {
-    const id = req.params.id
-    const index = id - 1 
-    const data = req.body    
-    todos[index] = {...todos[index], ...data}
-    res.send(todos[index])
-})
-
-app.delete('/todo/:id', (req, res) => {
-    const id = req.params.id
-    const index = id - 1        
-    todos.splice(index, 1)
-    res.send(todos)
-})
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
