db68566db153439b5d41a6c70bf1e449376e4c46
radiegtya <radiegtya@gmail.com>
2019-08-16 17:17:14 +0700

    10.sequelize-query

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,36 +1,21 @@
+const Todo = require('../models').todo
+
 exports.index = (req, res) => {
-    connection.query('SELECT A.*, B.name as created_by FROM todos A LEFT JOIN users B ON A.created_by = B.id', (err, rows)=> {
-        if (err) throw err
-      
-        res.send(rows)
-    })  
+    Todo.findAll().then(todos=>res.send(todos))
 }
 
 exports.show = (req, res) => {
-    connection.query(`SELECT * FROM todos WHERE id=${req.params.id}`, (err, rows)=> {
-        if (err) throw err
-      
-        res.send(rows[0])
-    })
+    //DO IT YOURSELF - MINI QUIZ 2
 }
 
 exports.store = (req, res) => {
-    const { title, is_done } = req.body    
-
-    connection.query(`INSERT INTO todos (title, is_done) VALUES ('${title}', '${is_done}')`, (err)=> {
-        if (err) throw err
-    })    
-
-    res.send({
-        success: true,
-        data: req.body
-    })
+    //DO IT YOURSELF - MINI QUIZ 2
 }
 
 exports.update = (req, res) => {
-    //DO IT YOURSELF - MINI QUIZ
+    //DO IT YOURSELF - MINI QUIZ 2
 }
 
 exports.delete = (req, res) => {
-    //DO IT YOURSELF - MINI QUIZ
+    //DO IT YOURSELF - MINI QUIZ 2
 }
